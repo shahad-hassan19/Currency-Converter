890c2f17419e346bda40d50b66c06e4bc4a2a94d
Shahad Hassan <shahadg1983@gmail.com>
2024-01-12 15:26:40 +0530

    updating styles and debugging functionaity

--- curr-converter/src/App.js
+++ curr-converter/src/App.js
@@ -22,7 +22,7 @@ function App() {
 
     useEffect(
         () => {
-            setConvertedAmount((amount * fromCurrencyInfo[to]).toFixed(4))
+            setConvertedAmount((amount * fromCurrencyInfo[to]).toFixed(2))
             setCopy('Copy')
             },[ amount, from, to ,fromCurrencyInfo]
     )
@@ -41,11 +41,12 @@ function App() {
     }
 
     return (
-        <div className="w-full min-h-full h-screen p-10 flex flex-col items-center bg-lime-950 bg-opacity-90" >
-            <h1 className=' text-5xl md:text-6xl text-center font-extrabold mt-10 mb-24' style={styles}>CoinQuotient</h1>
+        <div className="w-full min-h-full h-screen p-6 flex flex-col items-center bg-lime-950 bg-opacity-90" >
+            <h1 className=' text-5xl md:text-6xl text-center font-extrabold mt-5 mb-24' style={styles}>CoinQuotient</h1>
             <h2 className=' text-2xl md:text-3xl text-center font-extrabold mb-10' style={styles}> Convert { from.toUpperCase() } to { to.toUpperCase() } at the real exchange rate. </h2>
             <div className="w-full">
                 <div className="w-full sm:w-3/4 mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white">
+                <p className='text-xl text-center font-semibold mb-8'>{1} {from.toUpperCase()} = <span className=' text-green-700'>{(1 * fromCurrencyInfo[to]).toFixed(2)}</span> {to.toUpperCase()}</p>
                     <form
                         onSubmit={(e) => {
                         e.preventDefault();
@@ -67,7 +68,7 @@ function App() {
                                 type="button"
                                 className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 rotate-90 md:rotate-180 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
                                 onClick={swap} >
-                                <i class="fa-solid fa-arrow-right-arrow-left"></i>
+                                <i className="fa-solid fa-arrow-right-arrow-left"></i>
                             </button>
                         </div>
                         <div className="w-full shadow-md shadow-black rounded-md p-2">
@@ -81,8 +82,7 @@ function App() {
                             />
                         </div>
                         </div>
-                        <div className='mt-5 flex text-center justify-between flex-col md:flex-row'>
-                            <p className='text-xl font-semibold p-3'>{1} {from.toUpperCase()} = <span className=' text-green-700'>{fromCurrencyInfo[to]}</span> {to.toUpperCase()}</p>
+                        <div className='mt-5 flex justify-center md:justify-end'>
                             <button type="submit" className="sm:m-5 md:m-0 self-center lg:self-end w-32 bg-blue-600 text-white p-3 rounded-lg" onClick={handleClick}>
                                 {copy}
                             </button>
--- curr-converter/src/Hooks/useCurrencyInfo.js
+++ curr-converter/src/Hooks/useCurrencyInfo.js
@@ -7,6 +7,5 @@ export default function useCurrencyInfo(currency){
         .then((res) => res.json())
         .then((res) => setData(res[currency]))
     },[ currency ])
-    console.log(data);
     return data
 }
